767c7276822c83efac7b2d400317947de0aba3b7
PRBizarro <prbizarro@hotmail.com>
2017-09-04 20:11:14 -0300

    Primeiro commit app TestDrive

---
+++ TestDrive2/Views/DetalheView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestDrive2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalheView : ContentPage
+    {
+        public Veiculo Veiculo { get; set; }
+
+        public DetalheView(Veiculo veiculo)
+        {
+            InitializeComponent();
+            this.Veiculo = veiculo;
+
+            this.BindingContext = this;
+        }
+
+        private void buttonProximo_Clicked(object sender, EventArgs e)
+        {      
+            Navigation.PushAsync(new AgentamentoView(Veiculo));
+        }
+    }
+}
